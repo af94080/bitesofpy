a58065c2a609cee8403f5480416d970872126b36
af94080 <arulfranciswork@gmail.com>
2020-02-12 00:45:17 -0800

    submission Bite 106 @ codechalleng.es

--- 106/vowels.py
+++ 106/vowels.py
@@ -1,15 +1,32 @@
 from typing import Tuple
 import re
 
-text = """Hello world!
-We hope that you are learning a lot of Python.
-Have fun with our Bites of Py.
-Keep calm and code in Python!
-Become a PyBites ninja!
-All the way"""
+text = """
+The Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+"""
+
 vowels = 'aeiou'
 vowels_uc = "".join([ele.upper() for ele in vowels])
-
 vowels_pipe_delim = "|".join([ele for ele in vowels + vowels_uc])
 
 
