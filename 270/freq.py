782cdc8c3a27ea70c9969229532206ac2c289a5f
af94080 <arulfranciswork@gmail.com>
2020-10-31 19:09:51 -0700

    submission Bite 270 @ codechalleng.es

--- 270/freq.py
+++ 270/freq.py
@@ -1,8 +1,8 @@
 def freq_digit(num: int) -> int:
-    from collections import defaultdict
-    d = defaultdict(int)
-    for e in str(num):
-    	d[e] += 1
+	from collections import defaultdict
+	d = defaultdict(int)
+	for e in str(num):
+		d[e] += 1
 	d = sorted(d, key=d.get, reverse=True)
 	return int(d[0])
 	
