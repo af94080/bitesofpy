411fe1f99a579ca73faf8e3f579c0016ae702f3c
af94080 <arulfranciswork@gmail.com>
2020-11-16 00:38:46 -0800

    submission Bite 270 @ codechalleng.es

--- 279/armstrong.py
+++ 279/armstrong.py
@@ -5,3 +5,38 @@ def is_armstrong(n: int) -> bool:
     for char in n:
         tot += int(char) ** pwr
     return n == str(tot)
+
+def is_armstrong_dominic(n: int) -> bool:
+	'''1) Calling str(n) twice (reuse snum to compute size)
+		2) I don't think the list() conversion is necessary
+
+		Here's mine, 
+		similiar to <a href="#Martin">@Martin</a> 
+		but with a generator expression 
+		instead of a list:
+	'''
+    snum = str(n)
+    size = len(snum)
+    return sum(int(digit)**size for digit in snum) == n
+
+def is_armstrong_david_colton(n: int) -> bool:
+    return sum([int(num)**len(str(n)) for num in str(n)]) == n
+
+def is_armstrong_alex(n: int) -> bool:
+
+
+#### their solution: by daniel
+
+def is_armstrong_official_solution(n: int) -> bool:
+
+    snum = str(n)
+    size  = len(snum)
+
+    total = sum(
+        map(lambda x: x**size, map(int, snum))
+    )
+
+    return n == total
+
+    # or as a one-liner
+    # return n == sum(map(powered, map(int, snum)))
