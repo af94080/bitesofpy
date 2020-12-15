fd0122e92a69562bddb744fe63fff2bf2892f7bc
af94080 <arulfranciswork@gmail.com>
2020-12-15 14:38:42 -0800

    submission Bite 265 @ codechalleng.es

---
+++ 265/funds.py
@@ -0,0 +1,30 @@
+IMPOSSIBLE = 'Mission impossible. No one can contribute.'
+
+
+def max_fund(village):
+    """Find a contiguous subarray with the largest sum."""
+    # Hint: while iterating, you could save the best_sum collected so far
+    # return total, starting, ending
+    size = len(village)
+    st, en, poi = 0, 0, 0
+
+    curr_sum = 0
+    max_so_far = village[0]
+
+    for i in range(0, size):
+    	curr_sum = curr_sum + a[i]
+
+    	if max_so_far < curr_sum:
+    		max_so_far = curr_sum
+    		st = poi
+    		en = i
+
+		if (curr_sum < 0):
+			curr_sum = 0
+			poi=i+1
+
+	if max_so_far < 0:
+		return 0, 0, 0
+
+	return max_so_far, st+1, en+1
+	
